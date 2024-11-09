6ef27384898493e203f6093e8f08b75373438661
Mink <minkeiwu@gmail.com>
2024-11-09 22:30:48 +0800

    upload function

--- src/controllers/movie-controller.ts
+++ src/controllers/movie-controller.ts
@@ -3,6 +3,7 @@ import movie from "../models/movie-model";
 import mongoose from "mongoose";
 import { readdir, mkdir } from "node:fs/promises";
 import { join } from "node:path";
+import fs from 'fs';
 
 type TCategory = {
   name: string;
@@ -17,7 +18,7 @@ type TCategory = {
 }
 
 type TFile = {
-	uploadfile: File;
+  uploadfile: File;
 }
 
 const current: string = import.meta.dirname;
@@ -26,46 +27,91 @@ const controlPath: string = join(current, "../../", "public");
 export const MovieControllers = (app: Elysia) => {
 
   app.post("/movie", async (c) => {
-    const { name, category, actress, movieURL, sn, available, thumbURL, views, likes } = c.body as TCategory;
+    const { name, category, actress, sn, available, views, likes } = c.body as TCategory;
     const newMovie = await movie.create({
-      name, category, actress, movieURL, sn, available, thumbURL, views, likes
+      name,
+      category,
+      actress,
+      movieURL: `/data/public/movie/${sn}/movie.webm`,
+      sn,
+      available,
+      thumbURL: `/data/public/movie/${sn}/image.webm`,
+      views,
+      likes,
     });
     return newMovie
   });
 
   app.put("/movie/:id", async (c) => {
     const { id } = c.params;
-    const { name, category, actress, movieURL, sn, available, thumbURL, views, likes } = c.body as TCategory;
+    const { name, category, actress, sn, available, views, likes } = c.body as TCategory;
     const updateMovie = await movie.findByIdAndUpdate(id, {
-      name, category, actress, movieURL, sn, available, thumbURL, views, likes
+      name,
+      category,
+      actress,
+      movieURL: `/data/public/movie/${sn}/movie.webm`,
+      sn,
+      available,
+      thumbURL: `/data/public/movie/${sn}/image.webp`,
+      views,
+      likes
     }, { new: true })
     return updateMovie
   });
 
-  app.put("/data/movie/:sn", async (c)=> {
-		const { sn } = c.params;
-		const {uploadfile} = c.body as TFile;
-    let dirExist: boolean = false;
-
-		const imagePath = `${controlPath}/movie/${sn}/movie.webm`
-
-    console.log(imagePath)
+  app.put("/movie/movie/:sn", async (c) => {
+    const { sn } = c.params;
+    const { uploadfile } = c.body as TFile;
+    const singleMovie = await movie.find({ sn: sn });
+    const moviePath = `${controlPath}/movie/${singleMovie[0].sn}/`;
+    const movieName = `${moviePath}movie.webm`;
 
     try {
-      await readdir(imagePath);
-		  // await Bun.write(imagePath, uploadfile)
-      dirExist = true;
-      return {
-        response: "Dir exist."
-      }} catch (err) {
-        // await mkdir(imagePath);
-        // await Bun.write(imagePath, uploadfile)
-        return {
-          response: "Dir doesn't exist."
-        }
+      const stats = fs.statSync(moviePath);
+      if (stats.isDirectory()) {
+        await Bun.write(movieName, uploadfile)
+      } else {
+        console.log('路徑不是目錄');
       }
+    } catch (err) {
+      console.log('目錄不存在');
+      await mkdir(moviePath);
+      await Bun.write(movieName, uploadfile);
+    };
 
-	})
+    return {
+      success: true,
+      data: singleMovie,
+    };
+  });
+
+  app.put("/movie/thumb/:sn", async (c) => {
+    const { sn } = c.params;
+    const { uploadfile } = c.body as TFile;
+    const singleMovie = await movie.find({ sn: sn });
+    const imagePath = `${controlPath}/movie/${singleMovie[0].sn}/`;
+    const imageName = `${imagePath}image.webp`;
+
+    console.log("first")
+
+    try {
+      const stats = fs.statSync(imagePath);
+      if (stats.isDirectory()) {
+        await Bun.write(imageName, uploadfile);
+      } else {
+        console.log('路徑不是目錄');
+      }
+    } catch (err) {
+      console.log('目錄不存在');
+      await mkdir(imagePath);
+      await Bun.write(imageName, uploadfile);
+    };
+
+    return {
+      success: true,
+      data: singleMovie,
+    };
+  });
 
   app.get("/movie", async () => {
     const movies = await movie.find();
@@ -81,24 +127,9 @@ export const MovieControllers = (app: Elysia) => {
   app.delete("/movie/:id", async (c) => {
     const { id } = c.params;
     await movie.findByIdAndDelete(id, { new: true })
-  });
-
-  app.get("/movie/checkdir", async () => {
-    const path1 = `${process.env.PUBLIC_PATH}/image/banner`;
-    const path2 = `${process.env.PUBLIC_PATH}/image/anner`;
-
-    let exists = false;
-    try {
-      await readdir(path2);
-      exists = true;
-    } catch (err) { }
-
-    if (exists) {
-      return new Response(path2 + " exists.")
-    } else {
-      // await mkdir(path2, { recursive: true });
-      return new Response(path2 + " doesn't exist. /n And built now.")
-    }
+    return {
+      success: true,
+    };
   });
 
   return Promise.resolve(app)
--- src/controllers/upload-controller.ts
+++ src/controllers/upload-controller.ts
@@ -1,58 +1,112 @@
 import { Elysia } from "elysia";
 import act from "../models/activity-model";
+import mongoose from "mongoose";
+import { readdir, mkdir } from "node:fs/promises";
+import { join } from "node:path";
+import fs from 'fs';
 
-type TFile = {
+type TAct = {
   sn: string;
   title: string;
-  imagename: string;
-  filename: string;
   imagepath: string;
   filepath: string;
-  image: File;
-  file: File;
   views: number;
 }
+type TFile = {
+  uploadfile: File;
+}
+
+const current: string = import.meta.dirname;
+const controlPath: string = join(current, "../../", "public");
 
 export const UploadControllers = (app: Elysia) => {
-
+  // 讀取全部活動
   app.get("/upload", async ()=> {
     const acts = await act.find();
     return acts
   });
-
+  // 新增活動
   app.post("/upload", async (c) => {
-      const { sn, title, imagepath, filepath, image, imagename, file, filename, views } = c.body as TFile;
-
-      // if (image) {
-      //   await Bun.write(imagepath, image)
-      // }
-      
-      // if (file) {
-      //   await Bun.write(filepath, file);
-      // }
+      const { sn, title, imagepath, filepath, views } = c.body as TAct;
 
       const newAct = await act.create({
-        sn, title, filepath, imagepath, views
+        sn,
+        title,
+        filepath: `/data/public/act/${sn}/movie.webm`,
+        imagepath: `/data/public/act/${sn}/image.webp`,
+        views,
       })
 
     return newAct
   });
-
+  // 修改活動
   app.put("/upload/:id", async (c)=> {
     const { id } = c.params;
-    const { sn, title, imagepath, filepath, image, imagename, file, filename, views  } = c.body as TFile;
-
-    // if(image) {
-    //   const imagepath = `/data/public/${sn}/${imagename}`
-    //   await Bun.write(imagepath, image)
-    //   await act.findByIdAndUpdate(id, { imagepath}, {new: true})
-    // }
-    console.log(imagepath)
+    const { sn, title, imagepath, filepath, views } = c.body as TAct;
 
     const updateAct = await act.findByIdAndUpdate(id, {
-      sn, title, imagepath, filepath, image, imagename, file, filename, views
+      sn,
+      title,
+      filepath: `/data/public/act/${sn}/movie.webm`,
+      imagepath: `/data/public/act/${sn}/image.webp`,
+      views,
     }, {new: true})
     return updateAct
+  });
+
+  app.put("/upload/movie/:sn", async (c) => {
+    const { sn } = c.params;
+    const { uploadfile } = c.body as TFile;
+    const singleMovie = await act.find({ sn: sn });
+    const moviePath = `${controlPath}/act/${singleMovie[0].sn}/`;
+    const movieName = `${moviePath}movie.webm`;
+
+    try {
+      console.log(moviePath)
+      const stats = fs.statSync(moviePath);
+      if (stats.isDirectory()) {
+        await Bun.write(movieName, uploadfile)
+      } else {
+        console.log('路徑不是目錄');
+      }
+    } catch (err) {
+      console.log('目錄不存在');
+      await mkdir(moviePath);
+      await Bun.write(movieName, uploadfile);
+    };
+
+    return {
+      success: true,
+      data: singleMovie,
+    };
+  });
+
+  app.put("/upload/thumb/:sn", async (c) => {
+    const { sn } = c.params;
+    const { uploadfile } = c.body as TFile;
+    const singleMovie = await act.find({ sn: sn });
+    const imagePath = `${controlPath}/act/${singleMovie[0].sn}/`;
+    const imageName = `${imagePath}image.webp`;
+
+    console.log("first")
+
+    try {
+      const stats = fs.statSync(imagePath);
+      if (stats.isDirectory()) {
+        await Bun.write(imageName, uploadfile);
+      } else {
+        console.log('路徑不是目錄');
+      }
+    } catch (err) {
+      console.log('目錄不存在');
+      await mkdir(imagePath);
+      await Bun.write(imageName, uploadfile);
+    };
+
+    return {
+      success: true,
+      data: singleMovie,
+    };
   });
 
   app.get("/upload/:sn", async (c)=> {
@@ -64,6 +118,9 @@ export const UploadControllers = (app: Elysia) => {
   app.delete("/upload/:id", async (c)=> {
     const { id } = c.params;
     await act.findByIdAndDelete(id, {new: true})
+    return {
+      success: true,
+    };
   });
   
   return Promise.resolve(app)
